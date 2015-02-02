ae3c2ced4f14243ee91541228bd332aa301033cc
Eladio Caritos <eladio@caritos.com>
2015-02-02 14:35:13 -0500

    Add Student model.

---
+++ src/main/java/com/caritos/pangolin/domain/Student.java
@@ -0,0 +1,35 @@
+package com.caritos.pangolin.domain;
+
+/**
+ * Created by carel06 on 2/2/2015.
+ */
+public class Student {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
